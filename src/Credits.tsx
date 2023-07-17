46b644d1ba0cec6ccfcc13e8d70c2ea2e22fd259
Oğuz Kurt <kurt.oguz@outlook.com>
2023-07-18 00:18:23 +0300

    removed unnused <a> in credits

--- src/Credits.tsx
+++ src/Credits.tsx
@@ -8,7 +8,7 @@ function Credits() {
             <h2><a href="https://github.com/styrowolf/transcriptase">transcriptase</a></h2>
             
             <p>© 2023 Oğuz Kurt</p>
-            <p>an <a href="">application</a> for transcribing recordings powered by: </p>
+            <p>an application for transcribing recordings powered by: </p>
             <ul>
                 <li><a href="https://github.com/ggerganov/whisper.cpp">whisper.cpp</a></li>
                 <li><a href="https://github.com/tazz4843/whisper-rs">whisper-rs</a></li>
